474d63805fec55bf61e66fbc20807745919d7800
Diaa el din atya <diaa.atya@hotmail.com>
2021-02-03 23:30:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ app = FastAPI()
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
-@app.get("/")
+@app.get("/rates")
 def home():
-    return {"message":"Hello TutLinks.com"}
+    return {"USD":1,
+            "EGP":15.72,
+            "EUR":0.83,
+            "GBP":0.73,
+            "SAR":3.75,
+            "AED":3.67,
+            "KWD":0.30,
+           
+           }
