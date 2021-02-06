795f4589529b416644774557d3dd0d48de44153e
Diaa el din atya <diaa.atya@hotmail.com>
2021-02-06 13:14:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
-#last update 2/5/2021 8:20 AM CLT
+#last update 2/6/2021 1:00 PM
 @app.get("/rates")
 def home():
     return {"USD":1,
-            "EGP":15.67,
-            "EUR":0.84,
+            "EGP":15.68,
+            "EUR":0.83,
             "GBP":0.73,
             "SAR":3.75,
             "AED":3.67,
