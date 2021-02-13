9e024094cef49281ee08ec698ce61873bd70a1f1
diaaatya <diaa.atya@hotmail.com>
2021-02-13 10:01:21 +0200

    13-fep update

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-#last update 2/12/2021 6:41 AM :https://www.xe.com/
+#last update 2/13/2021 10:00 AM :https://www.xe.com/
 @app.get("/rates")
 def home():
     return {"USD":1,
-            "EGP":15.66,
+            "EGP":15.61,
             "EUR":0.82,
             "GBP":0.72,
             "SAR":3.75,
