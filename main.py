d044ac5dbdc5625cb882091cf0260913f828bf5a
diaaatya <diaa.atya@hotmail.com>
2021-02-14 08:39:42 +0200

    last update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/rates")
 def home():
     return {"USD":1,
-            "EGP":15.61,
+            "EGP":15.60,
             "EUR":0.82,
             "GBP":0.72,
             "SAR":3.75,
